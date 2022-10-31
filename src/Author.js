aaf0f3a9e09ab32a7a9db8139432525840bd4196
user <toropov.nikita2004@gmail.com>
2022-10-31 18:13:44 +0100

    Add surname

--- src/Author.js
+++ src/Author.js
@@ -2,7 +2,7 @@ import styles from "./css/Author.module.scss"
 const Author = () =>{
     return (
         <div className="creator">
-            <p className={styles.author}>by <a href="https://www.facebook.com/nikita.toropov.54" target="_blank">Nikita</a></p>
+            <p className={styles.author}>by <a href="https://www.facebook.com/nikita.toropov.54" target="_blank">Nikita Toropov</a></p>
         </div>
     );
 }
